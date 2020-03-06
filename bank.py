16e5e7f639fa3cd1d8ea611413666b7fe4dd3ef4
Hugo Brilhante <hugobrilhante@gmail.com>
2020-03-05 21:18:50 -0300

    Add model for accounts

---
+++ bank.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+
+
+class Account:
+    assessment: Decimal = Decimal(5)
+
+    def __init__(self, account_number: int, balance: int):
+        self.account_number: int = int(account_number)
+        self.balance: Decimal = Decimal(balance)
+
+    def deposit(self, value: Decimal):
+        self.balance += value
+
+    def debit(self, value: Decimal):
+        self.balance += value - self.assessment
